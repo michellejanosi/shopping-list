7cbb977864d4e27a48b670a996598ca0c3f7e7ec
michellejanosi <michelle.janosi@gmail.com>
2020-06-02 18:01:04 -0400

    Add functions to add, check, and remove items

--- index.js
+++ index.js
@@ -5,8 +5,33 @@
 
 function main() {
   $('#js-shopping-list-form').submit(event => {
-    event.preventDefault;
+    event.preventDefault();
 
+    let entry = $('#shopping-list-entry').val();
+    let listEntry = `<li>
+        <span class="shopping-item">${entry}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`;
+
+    $('.shopping-list').append(listEntry);
+    $('#shopping-list-entry').val('');
+
+    $('ul .shopping-item-toggle').click(function (event) {
+      event.stopPropagation();
+
+      $(this).parent().siblings('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
+    $('ul .shopping-item-delete').click(function () {
+      $(this).closest('li').remove();
+    });
   });
 }
 
