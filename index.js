2a33e8b364f16147b23bc3bada74844a8d30c0ae
michellejanosi <michelle.janosi@gmail.com>
2020-06-02 15:14:06 -0400

    Add jquery and boilerplate

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+// app user should be able to enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+// check and uncheck items on the list by clicking the "Check" button
+// permanently remove items from the list
+
+function main() {
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault;
+
+  });
+}
+
+$(main);
